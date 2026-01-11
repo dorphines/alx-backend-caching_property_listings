d7a66a8d8bd75e16b7803f1415760e6b718c901f
dorphines <dorphines@gmail.com>
2026-01-11 17:48:03 +0300

    Calculated and returned hit_ratio

--- properties/utils.py
+++ properties/utils.py
@@ -26,8 +26,8 @@ def get_redis_cache_metrics():
         hits = int(info.get('keyspace_hits', 0))
         misses = int(info.get('keyspace_misses', 0))
         
-        total = hits + misses
-        hit_ratio = hits / total if total > 0 else 0
+        total_requests = hits + misses
+        hit_ratio = hits / total_requests if total_requests > 0 else 0
         
         metrics = {
             'hits': hits,
